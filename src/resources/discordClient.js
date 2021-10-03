20880a6dd82658379d0d82f376dfe9eaf6aa2be2
Suraj Bobade <iamsurajbobade@gmail.com>
2021-10-03 07:30:11 +0000

    Updated packages

--- src/resources/discordClient.js
+++ src/resources/discordClient.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
-const intents = new Discord.intents(32767);
 
-const client = new Discord.client({ intents });
+const client = new Discord.client();
 
 module.exports = { client };
